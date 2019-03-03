28bea8f5c34ac423fcea072ee7ccaac6adb11078
racheldallaire <rachel-dallaire@outlook.com>
2019-03-02 21:54:48 -0500

    as a user, I can create a company

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
+import Grid from '@material-ui/core/Grid';
+
 import './App.css';
-import { Header } from './Header/Header';
-import { Dashboard } from './Dashboard/Dashboard';
+import Header from './Header/Header';
+import Dashboard from './Dashboard/Dashboard';
+
 
 class App extends Component {
     render() {
         return (
             <div>
                 <Header />
-                <Dashboard />
+                <Grid container spacing={24}>
+                    <Grid item xs />
+                    <Grid item xs={6}>
+                        <Dashboard />
+                    </Grid>
+                    <Grid item xs />
+                </Grid>
             </div>
         );
     }
