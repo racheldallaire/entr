53d7a1bc6e36bc65618938d70ebfa7d3e4cd0244
racheldallaire <rachel-dallaire@outlook.com>
2019-03-04 19:40:40 -0500

    testing heroku deployment

---
+++ build/precache-manifest.6eab1f45177add8ade2c4184553059e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "7a8a08d0ace7bdbd63df",
+    "url": "/static/js/main.7a8a08d0.chunk.js"
+  },
+  {
+    "revision": "1127b4814e608aef6efc",
+    "url": "/static/js/2.1127b481.chunk.js"
+  },
+  {
+    "revision": "7a8a08d0ace7bdbd63df",
+    "url": "/static/css/main.b8956a32.chunk.css"
+  },
+  {
+    "revision": "5c1ef11df8260d8ab31bfb472aea2c33",
+    "url": "/index.html"
+  }
+];
